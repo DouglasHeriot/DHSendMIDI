f8706df6ae6c2081cf3ddf15bdb2576f107f976e
Douglas Heriot <douglas@douglasheriot.com>
2013-01-23 07:34:15 +1100

    Initial Commit

---
+++ DHSendMIDI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DHSendMIDI
+//
+//  Created by Douglas Heriot on 23/01/13.
+//  Copyright (c) 2013 Douglas Heriot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
